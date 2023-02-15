19725a152eec7321d31488fff23bf43019bae03f
Florencia Guardia <guardiaflorencia@gmail.com>
2023-02-15 19:26:21 -0300

    proyecto al 95%

---
+++ script/script.js
@@ -0,0 +1,17 @@
+
+let texto_btn = document.getElementById('texto_btn')
+
+let texto = document.getElementById('texto')
+
+texto_btn.addEventListener('click', cambiarStyle); 
+
+function cambiarStyle() {
+    texto.classList.toggle('mostrar') 
+
+    if  (texto.classList.contains('mostrar')) {
+        texto_btn.innerHTML= 'Ver menos'; 
+    } else {
+    texto_btn.innerHTML= 'Ver mas';
+    }
+}
+
